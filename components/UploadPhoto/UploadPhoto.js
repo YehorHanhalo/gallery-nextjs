ef7d734800108b1b803324f9aabe9f2ede64898d
Yegor Gangalo <gangaloyegor@gmail.com>
2021-10-16 23:12:14 +0300

    add debounced inputs for title and description. add redux logic in component UploadPhoto

--- components/UploadPhoto/UploadPhoto.js
+++ components/UploadPhoto/UploadPhoto.js
@@ -1,53 +1,76 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import { useSnackbar } from 'notistack'
+import { DebounceInput } from 'react-debounce-input';
 import { useDispatch, useSelector } from 'react-redux'
-import { editHeader } from '../../store/header/header-actions'
-import { editDescription } from '../../store/description/description-actions'
-import { getEditHeader } from '../../store/header/header-selector'
-import { getEditDescription } from '../../store/description/description-selector'
+import { postTitle, fetchTitle } from '../../store/title/title-operations'
+import { postDescription, fetchDescription } from '../../store/description/description-operations'
+import { deleteAllPhotos } from '../../store/photo/photo-operations'
+import { getTitle, getTitleError } from '../../store/title/title-selector'
+import { getDescription, getDescriptionError } from '../../store/description/description-selector'
 
 import DragAndDrop from '../DragAndDrop'
-import API from '../../api'
 import styles from './UploadPhoto.module.scss'
 
-const UploadPhoto = () => {
+const UploadPhoto = ({ setPageAmount }) => {
     const { enqueueSnackbar } = useSnackbar()
     const dispatch = useDispatch()
-    const headerText = useSelector(getEditHeader)
-    const descriptionText = useSelector(getEditDescription)
+    const title = useSelector(getTitle)
+    const titleError = useSelector(getTitleError)
+    const description = useSelector(getDescription)
+    const descriptionError = useSelector(getDescriptionError)
+
+    useEffect(() => {
+        dispatch(fetchTitle())
+        dispatch(fetchDescription())
+    }, [])
+
+    useEffect(() => {
+        const error = titleError || descriptionError
+        if (error) {
+            enqueueSnackbar(`Something wrong. ${error}`, { variant: "error"})
+        }
+    }, [titleError, descriptionError])
+
+    const successNotification = () => enqueueSnackbar('All files are deleted', {variant: 'success'})
 
     const handleDeleteAllPhotos = async () => {
         try {
-            const result = await API.deleteAllPhotos()
+            dispatch(deleteAllPhotos({ successNotification }))
         } catch (error) {
+            console.log(error)
             enqueueSnackbar('Files are not deleted. try again', {variant: 'error'})
         }
     }
 
     const handleChangeHeaderValue = (e) => {
         const value = e.target.value
-        dispatch(editHeader(value))
+        dispatch(postTitle(value))
     }
 
     const handleChangeDescriptionValue = (e) => {
         const value = e.target.value
-        dispatch(editDescription(value))
+        dispatch(postDescription(value))
     }
 
     return (
         <section className={styles.root}>
-            <input
+            <DebounceInput
+                value={title}
                 className={styles.textBlock}
-                value={headerText}
+                debounceTimeout={300}
                 onChange={handleChangeHeaderValue}
             />
-            <textarea
-                className={styles.textarea}
+            <DebounceInput
+                element="textarea"
+                value={description}
                 rows="4"
-                value={descriptionText}
+                className={styles.textarea}
+                debounceTimeout={300}
                 onChange={handleChangeDescriptionValue}
             />
-            <DragAndDrop />
+            <DragAndDrop
+                setPageAmount={setPageAmount}
+            />
             <button
                 onClick={handleDeleteAllPhotos}
                 className={styles.button}
