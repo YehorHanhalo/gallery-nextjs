72e69b5995617102832a2cc5ef08d2bff4821c38
Yegor Gangalo <gangaloyegor@gmail.com>
2021-10-16 23:14:13 +0300

    add api requests for title and description

--- api/index.js
+++ api/index.js
@@ -4,14 +4,30 @@ const api = axios.create({
   baseURL: process.env.API_URL,
 });
 
-export const getAllPhotos = () => {
-  return api.get('/getAll')
+export const getAllPhotos = (page, limit = 9) => {
+  return api.get(`/api/photo?page=${page}&limit=${limit}`)
 }
 
 export const postPhoto = (formData) => {
-  return api.post('/add', formData)
+  return api.post('/api/photo', formData)
 }
 
 export const deleteAllPhotos = () => {
-  return api.delete('/deleteAll')
+  return api.delete('/api/photo')
+}
+
+export const getTitle = () => {
+  return api.get('/api/title')
+}
+
+export const postTitle = (newTitle) => {
+  return api.post('/api/title', { title: newTitle })
+}
+
+export const getDescription = () => {
+  return api.get('/api/description')
+}
+
+export const postDescription  = (newDescription ) => {
+  return api.post('/api/description', { description: newDescription })
 }
