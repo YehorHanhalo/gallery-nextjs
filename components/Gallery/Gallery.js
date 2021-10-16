f2c65abbee7a61ba89208278fc7616f7e7cb87b5
Yegor Gangalo <gangaloyegor@gmail.com>
2021-10-16 23:19:45 +0300

    add Gallery redux logic, photos mark up, buttons, set pageAmount state upper in Index page

--- components/Gallery/Gallery.js
+++ components/Gallery/Gallery.js
@@ -1,30 +1,80 @@
-import { useSelector } from 'react-redux'
-import { getEditHeader } from '../../store/header/header-selector'
-import { getEditDescription } from '../../store/description/description-selector'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getTitle, getTitleLoading } from '../../store/title/title-selector'
+import { getDescription, getDescriptionLoading } from '../../store/description/description-selector'
+import { fetchPaginatedPhotos } from '../../store/photo/photo-operations'
+import { getAllPhoto } from '../../store/photo/photo-selector'
+import IconButton from '../IconButton';
 import styles from './Gallery.module.scss'
 
-const Gallery = () => {
-    const headerText = useSelector(getEditHeader)
-    const descriptionText = useSelector(getEditDescription)
+const Gallery = ({ setPageAmount, pageAmount }) => {
+    const dispatch = useDispatch()
+    const title = useSelector(getTitle)
+    const titleLoading = useSelector(getTitleLoading)
+    const description = useSelector(getDescription)
+    const descriptionLoading = useSelector(getDescriptionLoading)
+    const photos = useSelector(getAllPhoto)
+
+    const [page, setPage] = useState(0)
+
+    const handleClickHomeButton = () => setPage(0);
+    const handleClickIncrementPageButton = () => setPage(prev => prev < pageAmount - 1 ? prev + 1 : prev);
+    const handleClickDecrementPageButton = () => setPage(prev => prev > 0 ? prev - 1 : prev);
+
+    const isIncrementButtonDisabled = page >= pageAmount - 1
+    const isDecrementButtonDisabled = page <= 0
+
+    useEffect(() => {
+        if (page < pageAmount) {
+            dispatch(fetchPaginatedPhotos({ page, setPageAmount }))
+        }
+    }, [page, pageAmount])
 
     return (
         <section className={styles.root}>
-            <h1 className={styles.header}>{headerText}</h1>
-            <p className={styles.description}>{descriptionText}</p>
-            <ul>
-                <li>
-                    <img />
-                </li>
+            <h1 className={styles.header}>{`${title}${titleLoading ? '...' : ''}`}</h1>
+            <p className={styles.description}>{`${description}${descriptionLoading ? '...' : ''}`}</p>
+            <ul className={styles.galleryList}>
+                {photos.map(photo => (
+                    <li
+                        key={photo}
+                        className={styles.photoItem}
+                    >
+                        <img
+                            loading="lazy"
+                            src={`${process.env.API_URL}/${photo}`}
+                            className={styles.photo}
+                            width="200px"
+                        />
+                    </li>
+                ))}
             </ul>
-            <ul>
-                <li>
-                    <button></button>
+            <ul className={styles.galleryList}>
+                <li className={styles.galleryItem}>
+                    <IconButton
+                      onClick={handleClickDecrementPageButton}
+                      aria-label="previous page"
+                      disabled={isDecrementButtonDisabled}
+                    >
+                      <i className="ri-arrow-left-line ri-xl" />
+                    </IconButton>
                 </li>
-                <li>
-                    <button></button>
+                <li className={styles.galleryItem}>
+                    <IconButton
+                      onClick={handleClickHomeButton}
+                      aria-label="first page"
+                    >
+                      <i className="ri-home-line ri-xl" />
+                    </IconButton>
                 </li>
-                <li>
-                    <button></button>
+                <li className={styles.galleryItem}>
+                    <IconButton
+                      onClick={handleClickIncrementPageButton}
+                      aria-label="next page"
+                      disabled={isIncrementButtonDisabled}
+                    >
+                      <i className="ri-arrow-right-line ri-xl" />
+                    </IconButton>
                 </li>
             </ul>
         </section>
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,25 @@
+import { useState } from 'react'
 import MainContainer from "../components/MainContainer";
 import UploadPhoto from "../components/UploadPhoto";
 import Gallery from '../components/Gallery'
 import styles from '../styles/index.module.scss'
 
 const Index = () => {
+  const [pageAmount, setPageAmount] = useState(1)
+
   return (
     <MainContainer>
       <ul className={styles.list}>
         <li className={styles.listItem}>
-          <UploadPhoto/>
+          <UploadPhoto
+            setPageAmount={setPageAmount}
+          />
         </li>
         <li className={styles.listItem}>
-          <Gallery />
+          <Gallery
+            setPageAmount={setPageAmount}
+            pageAmount={pageAmount}
+          />
         </li>
       </ul>
     </MainContainer>
